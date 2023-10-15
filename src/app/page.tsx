5bcefec3916aba7fe26f3786c859b172369be05a
Muhammad Shahbaz <muhamadshahbaz17@gmail.com>
2023-10-15 10:46:17 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p className={`text-red-400 font-bold font-bold text-7xl`}>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <p className={`text-red-400 font-bold font-bold text-7xl text-center`}>
         Hello Vercel Folks!
       </p>
     </main>
